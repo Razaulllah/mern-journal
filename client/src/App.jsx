d120be2a3fb2ef9d310501573d0958adf9ce6e1b
Raza Ullah <razaullah833@gmail.com>
2024-02-17 09:21:10 +0500

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,18 @@ import SignIn from './pages/SignIn'
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/rojects" element={<Projects />} />
-      
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
