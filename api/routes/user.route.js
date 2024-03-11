65e16a485c169672646a33627ee7d880aa34842c
Raza Ullah <razaullah833@gmail.com>
2024-03-11 07:39:10 +0500

    complete delete user account api and in client side also

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { UpdateUser, test } from "../controllers/user.controller.js";
+import {
+  UpdateUser,
+  deleteUser,
+  test,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, UpdateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
