d7b77c193c58dd0036f85d8e4c1039962c89f03a
Raza Ullah <razaullah833@gmail.com>
2024-03-22 16:08:34 +0500

    add dashborad overview to the admin dashboard

--- client/src/components/DashComments.jsx
+++ client/src/components/DashComments.jsx
@@ -89,7 +89,9 @@ export default function DashComments() {
                   <Table.Cell>
                     {new Date(comment.updatedAt).toLocaleDateString()}
                   </Table.Cell>
-                  <Table.Cell>{comment.content}</Table.Cell>
+                  <Table.Cell className="w-80">
+                    <p className="line-clamp-2">{comment.content}</p>
+                  </Table.Cell>
                   <Table.Cell>{comment.numberOfLikes}</Table.Cell>
                   <Table.Cell>{comment.postId}</Table.Cell>
                   <Table.Cell>{comment.userId}</Table.Cell>
