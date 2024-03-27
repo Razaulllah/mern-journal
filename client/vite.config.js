7f3f7f26994fc4f1c50db0511431ce07a43e9583
Raza Ullah <razaullah833@gmail.com>
2024-03-28 00:20:10 +0500

    changes for delpoy in vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; //removed swc
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
       },
     },
   },
   plugins: [react()],
+  build: {
+    outDir: "public",
+  },
 });
