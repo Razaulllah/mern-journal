3d85c3f8b2f0312e62794405d3770ad99b561d90
Razaulllah <52465460+Razaulllah@users.noreply.github.com>
2024-03-28 22:13:15 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,13 +5,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "window.location.origin",
         secure: false,
       },
     },
   },
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+  plugins: [react()]
 });
