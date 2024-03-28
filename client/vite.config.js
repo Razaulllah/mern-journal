9ed70dfb27fa8579fd44688f59e03d0d0d7a6f79
Razaulllah <52465460+Razaulllah@users.noreply.github.com>
2024-03-28 08:41:51 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-journal-h5ik.vercel.app",
         secure: false,
       },
     },
